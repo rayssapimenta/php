fbec793e741d9a7b8c71f04d65494a8c2cedd6fa
Rayssa <ralpimenta@hotmail.com>
2023-01-24 23:16:01 -0300

    Exercicio de while

--- estruturas/repeticao/ex1.php
+++ estruturas/repeticao/ex1.php
@@ -2,7 +2,15 @@
 //criar array com dados mistos e crie um while pra imprimir os que são strings
 
 $arr = [25,'oi',44,'ok',true,'eu'];
+$arr_valores = count($arr);
 
-while (is_string($arr[0])){
-    echo 'string';
+
+$i = 0;
+//echo $arr[$i];
+while  ($i < $arr_valores) {
+    if(is_string($arr[$i]) == true)
+        echo $arr[$i] .' -> É string.'. "\n";
+    else 
+        echo $arr[$i].' -> Não é string.'. "\n";
+    $i++;
 }
