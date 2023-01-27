6283e5d966b50a4846f03105ee8fdf82f2218d8a
Rayssa <ralpimenta@hotmail.com>
2023-01-26 23:30:43 -0300

    Usando loops encadeados para realizar tabuadas

--- estruturas/repeticao/loopAninhado.php
+++ estruturas/repeticao/loopAninhado.php
@@ -1,10 +1,17 @@
 <?php
 
 //similar ao if aninhado
+//contador é por loop para n misturar os valores
+//a quantia de vezes de execuçao do interno é a referente ao externo
+$multiplicando = 1;
 
 
-while(){
-    while(){
-        
+while($multiplicando <= 10){
+    $multiplicador = 1;
+    while($multiplicador <= 10){
+        $multiplicacao = $multiplicando*$multiplicador;
+        echo $multiplicando.' X '.$multiplicador. " = $multiplicacao \n";
+        $multiplicador ++;
     }
+    $multiplicando++;
 }
