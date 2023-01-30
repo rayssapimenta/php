bfb165758dc36d863500e0df2d194fd1344991c1
Rayssa <ralpimenta@hotmail.com>
2023-01-30 16:42:16 -0300

    Testando tag resumida

---
+++ intro/resumoExibicao.php
@@ -0,0 +1,11 @@
+<?php
+    //usar 'shortag' p exibir algo no form .. ou qq outro lugar
+    /* <?=?>*/
+    $nome = "Rayssa";
+    $sobrenome = "Nunes";
+?>
+
+<form action="" method="get">
+        <input type="text" value="<?=$nome;?>">
+        <input type="text" value="<?=$sobrenome;?>">
+</form>
