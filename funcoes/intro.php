dc0de8f025483f1cc763fb4276b5d43e57eb4a0d
Rayssa <ralpimenta@hotmail.com>
2023-01-30 17:39:19 -0300

    intro a função

---
+++ funcoes/intro.php
@@ -0,0 +1,21 @@
+<?php
+//blocos que possuem nomes
+//realizam ações e podem ser reaproveitadas
+//podemos passar parametros, que moldam a sua execução
+//reduz duplicidade
+//melhora a manutenção
+//o PHP Já tem mts funções que podemos usar, são chamadas de built in
+//se for chamada, então será executada
+
+
+
+
+
+//DECLARANDO FUNÇÃO
+function teste(){
+    //...code
+    echo "oi";
+};
+
+//CHAMANDO/INVOCANDO FUNÇÃO
+teste();
