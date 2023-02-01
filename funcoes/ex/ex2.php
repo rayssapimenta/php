5e010b68947f2eead9be82651214ba188063ae80
Rayssa <ralpimenta@hotmail.com>
2023-01-31 23:42:48 -0300

    exercicio

---
+++ funcoes/ex/ex2.php
@@ -0,0 +1,13 @@
+<?php
+
+//crie uma função com tres var numericos e exiba sua multiplicação
+
+function multiplicacao(){
+    $a = 25;
+    $b = 15;
+    $c = 1;
+    echo $a * $b * $c;
+};
+
+
+multiplicacao();
---
+++ funcoes/ex/ex3.php
@@ -0,0 +1,10 @@
+<?php
+
+function concatenar(){
+    $nome = "Jubileu ";
+    $sobrenome = "da Silva";
+
+    echo $nome . $sobrenome ;
+
+};
+concatenar();
