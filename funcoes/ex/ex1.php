3cce57ca0ef656fa366cc9335696934f5bdd1f2a
Rayssa <ralpimenta@hotmail.com>
2023-01-31 23:35:54 -0300

    exercicio

---
+++ funcoes/ex/ex1.php
@@ -0,0 +1,14 @@
+<?php
+//array com strings
+//usar função implode no array
+//primeiro argumento: ","
+//segundo argumento: o seu array
+//atribua a invocação da função a uma variável
+//exiba o resultado
+
+
+$arr = ["teste", "teste1", "teste2"];
+
+$teste = implode(",", $arr);
+
+echo $teste;
