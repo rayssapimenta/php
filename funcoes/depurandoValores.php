198df2ea6ea5c5f8057dbd61bc2bf70fd2cf9cdb
Rayssa <ralpimenta@hotmail.com>
2023-02-02 16:58:52 -0300

    usando var_dump e o print_r

---
+++ funcoes/depurandoValores.php
@@ -0,0 +1,11 @@
+<?php
+//há 2 funções p/ ver dados
+// print_r e var_dump
+
+$humanos =[
+    'Nome' => "Rayssa",
+    'Idade' => 24
+];
+print_r($humanos);
+echo "\n";
+var_dump($humanos);//human readable - mais bonitinho
