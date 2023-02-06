026fe09164138284c4cf2452207e29e7d3aaa37d
Rayssa <ralpimenta@hotmail.com>
2023-02-06 15:56:04 -0300

    Manipulando o case de palavras, frases

---
+++ strings/alterandoCase.php
@@ -0,0 +1,13 @@
+<?php
+
+//tb podemos colocar tudo maiusculo ou minusculo
+// strtolower -> minusculo
+// strtoupper -> maiuscula
+
+$nome = "Rayssa";
+
+echo $nome;// sem modificar
+echo "\n";
+echo strtolower($nome);// td minusculo
+echo "\n";
+echo strtoupper($nome);// td maiusculo
---
+++ strings/alterandoCasePalavra.php
@@ -0,0 +1,18 @@
+<?php
+// mexe só na primeira letra da Palavra
+$teste = "deixa eu testar";
+
+echo ucfirst($teste);
+
+//primeira letra da palavra minusc
+$teste2 = "Deixa eu testar";
+
+echo "\n";
+
+echo lcfirst($teste2);
+
+echo "\n";
+
+//primeira letra maiuscula de cada palavra
+
+echo ucwords($teste);
