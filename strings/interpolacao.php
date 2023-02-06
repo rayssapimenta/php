7f1a55cc10a37e7405e27239b6b494c074f8c8dc
Rayssa <ralpimenta@hotmail.com>
2023-02-05 21:14:54 -0300

    Exemplos de interpolação no php

---
+++ strings/interpolacao.php
@@ -0,0 +1,10 @@
+<?php
+//Formas de interpolar variáveis em textos
+
+// usando "aspas duplas $var", e pondo entre chaves tb "tipo assim {$var}";
+
+//O ideal é sempre se manter com uma das sintaxes 
+
+$var = 'interpolar';
+
+echo "Não existe diferença na forma de {$var}";
