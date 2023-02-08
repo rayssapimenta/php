75a284748e70b725b312d8246a571217460995d0
Rayssa <ralpimenta@hotmail.com>
2023-02-07 23:35:56 -0300

    Usando função range pra criar array

---
+++ Arrays/criarArrayRapido.php
@@ -0,0 +1,14 @@
+<?php
+//range(inicio,fim,step);
+//step -> é a variação entre um e outro
+
+$arr = range(1, 10);
+
+print_r($arr);
+
+
+echo "\n";
+
+$arr2 = range(1, 100, 10);//vai de 1 a 100 de 10 em 10
+ 
+print_r($arr2);
