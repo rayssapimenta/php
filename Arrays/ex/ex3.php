38ca78cc24476df18c8ef011e293fdd8871b8410
Rayssa <ralpimenta@hotmail.com>
2023-02-08 02:29:44 -0300

    Exercicios

---
+++ Arrays/ex/ex3.php
@@ -0,0 +1,8 @@
+<?php
+//Crie um array com os seguintes valores: jaguar, 3.0, azul, 18, Teto solar, automático
+//chame este array de carro
+//Crie variaveis com base neste array
+
+$carro = ["jaguar", 3.0, "azul", 18, "Teto solar"];
+
+list($modelo, $potencia, $cor, $ano, $carac) = $carro;
