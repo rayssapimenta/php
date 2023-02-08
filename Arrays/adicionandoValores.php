7fca9c46c00bea387b8b67bf5fbbcdd049b1c66c
Rayssa <ralpimenta@hotmail.com>
2023-02-07 23:16:05 -0300

    Atribuindo valores a arrays

---
+++ Arrays/adicionandoValores.php
@@ -0,0 +1,10 @@
+<?php
+
+//podemos criar novos indices com dados em um array
+//basta por o nome do array com o novo indice em colchetes e atribuir o valor
+
+$arr[1] = "teste";
+
+//em arrays associativos basta utilizar o nome da nova chave com a atribuição de valor
+
+$arrAssoc['carro'] = 'BMW';
