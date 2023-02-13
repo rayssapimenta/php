4e72e9cdd3cd9f217ae36452444f87a8d0b203b9
Rayssa <ralpimenta@hotmail.com>
2023-02-13 02:35:44 -0300

    Declarando propriedades, acessando-as e modificando-as

--- OO/class.php
+++ OO/class.php
@@ -2,10 +2,18 @@
 //criando uma classe 
 class Pessoa {
     //propriedades
+    public $idade = 0; // declarar prop publica
     //metodos
     function falar(){
-        echo 'oi';
+        echo "oi\n";
     }
 }
 $Rayssa = new Pessoa; //instanciando um objeto 
+
 $Rayssa -> falar(); // acessando o método
+
+//echo $Rayssa->idade; //acessando uma prop
+
+$Rayssa->idade = 24; //alterando a propriedade
+
+echo $Rayssa->idade;
