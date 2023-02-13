2f158199db364aded0096c8c81cdbd4049454195
Rayssa <ralpimenta@hotmail.com>
2023-02-13 02:08:38 -0300

    Vendo como criar metodo e acessar

--- OO/class.php
+++ OO/class.php
@@ -3,5 +3,9 @@
 class Pessoa {
     //propriedades
     //metodos
+    function falar(){
+        echo 'oi';
+    }
 }
 $Rayssa = new Pessoa; //instanciando um objeto 
+$Rayssa -> falar(); // acessando o método
