0bb916a2aac6ecc52e48dd8a69716607df215e5e
Rayssa <ralpimenta@hotmail.com>
2023-02-16 23:37:05 -0300

    ex

--- OO/ex/ex3.php
+++ OO/ex/ex3.php
@@ -1,4 +1,22 @@
 <?php
 //crie uma classe Caroo;
 //crie algumas prop e também a propriedade velocidade_maxima
-//e tb o getVelocidadeMaxima onde é possivel imprimir a velocidade do carro
+//crie o metodo setVelocidadeMaxima
+//e tb o getVelocidadeMaxima onde é possivel imprimir a velocidade
+class Carro{
+    public $velocidade_maxima;
+    
+
+    function setVelocidadeMaxima($velocidade){
+        $this -> velocidade_maxima = $velocidade;
+        
+    }
+    function getVelocidadeMaxima(){
+        echo "Está é a velocidade máxima : {$this->velocidade_maxima}";
+    }
+
+}
+$gol = new Carro;
+
+$gol->setVelocidadeMaxima(100);
+$gol->getVelocidadeMaxima();
