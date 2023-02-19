2901501b96fbcaecef4769d6a6bc07408960a84b
Rayssa <ralpimenta@hotmail.com>
2023-02-19 19:37:57 -0300

    exemplo de constante

---
+++ OO/constantes.php
@@ -0,0 +1,9 @@
+<?php
+//constantes em classes
+//São parecidas com váriávies, salvam valores em memoria
+//porém, o seu valor não podera ser alterado
+//ex:
+//  public const CHAVE_API = "ASO793mJJs39";
+const nome = 'Ana';
+
+echo nome;
