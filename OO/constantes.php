9f3e09d132de3401e6e850396625bc6e8c1e855f
Rayssa <ralpimenta@hotmail.com>
2023-02-27 22:48:46 -0300

    exemplo para acessar as constantes como parametros e metodos

--- OO/constantes.php
+++ OO/constantes.php
@@ -4,6 +4,30 @@
 //porém, o seu valor não podera ser alterado
 //ex:
 //  public const CHAVE_API = "ASO793mJJs39";
-const nome = 'Ana';
+const NOME = 'Ana'."\n";; // Sempre maiúsculas
 
+<<<<<<< HEAD
 echo nome;
+=======
+echo NOME;
+
+//No caso de classes, as constantes são acessadas por :: ao invés de uma ->$_COOKIE
+
+class Pessoa{
+    public const OLHOS = 2;
+
+
+//no caso de metodos, se usa self no lugar de this;
+function mostrarConstante(){
+    echo self::OLHOS."\n";
+}
+
+
+}
+
+$Rayssa = new Pessoa;
+
+echo $Rayssa::OLHOS."\n";;
+
+$Rayssa -> mostrarConstante();
+>>>>>>> 8f8b087 (exemplo para acessar as constantes como parametros e metodos)
