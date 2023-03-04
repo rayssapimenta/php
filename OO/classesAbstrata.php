eb1bf9656839b30203b3e54b6d8970c00d979a48
Rayssa <ralpimenta@hotmail.com>
2023-03-03 23:55:15 -0300

    Classe abstrata

---
+++ OO/classesAbstrata.php
@@ -0,0 +1,8 @@
+<?php
+// são classes que não podem ser instanciadas
+//podemos ter métodos abstratos que devem ser implementados obrigatoriamente se uma classe herdar a abstrata;
+//palavra reservada : abstract
+//abstract class ClasseAbstrata{}
+
+
+?>
